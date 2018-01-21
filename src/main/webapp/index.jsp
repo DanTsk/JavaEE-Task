13f9f00ca2f44b333bda466594dfa0bf6e0a7226
DanTsk <tsekhmastryuk@gmail.com>
2018-01-21 22:21:40 +0200

    Task 1

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+	<body>
+		<form method="post" action="login">
+			<input type="text" name="username" placeholder="Username" />
+			<input type="password" name="password" placeholder="Password" /> 
+			<br>
+			<button type="submit">Login</button>
+		</form>	
+	
+	</body>
+</html>
---
+++ tsk/HomeServlet.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomeServlet extends HttpServlet {
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {  
+	       
+	    String username = request.getParameter("username");  
+	    String password = request.getParameter("password");  
+	  
+	    PrintWriter out = response.getWriter();   
+	    response.setContentType("text/html");  
+	    
+	    if(username.equals("admin") && password.equals("admin")){
+	    	this.writeMessage(out, "You successfully logged !");
+	    }else{  
+	    	this.writeMessage(out, "Error occurred, wrong credentials !");
+	    }   
+	    
+	    out.close();  
+	}  
+	
+	
+	private void writeMessage(PrintWriter out, String message){
+		out.println("<html>");
+
+		out.println("<head>");
+
+		out.println("<title>Â³äïîâ³äü</title>");
+
+		out.println("</head>");
+
+		out.println("<body>");
+
+		out.println(message);
+
+		out.println("</body>");
+
+		out.println("</html>");
+	}	
+}  	
+
